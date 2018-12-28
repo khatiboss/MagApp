b1b2e0c7afb1cdddf9ebed341e27cdc7bc38dcb1
Salah-eddine KHATIB <salaheddine.khatib@gamil.com>
2018-12-28 15:20:07 +0100

    Configurazione del DbContext

--- WebAPI/Models/IdentityModels.cs
+++ WebAPI/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -29,5 +30,8 @@ namespace WebAPI.Models
         {
             return new ApplicationDbContext();
         }
+
+        public IDbSet<Componente> Componenti { get; set; }
+        public IDbSet<Carrello> Carrelli { get; set; }
     }
 }
