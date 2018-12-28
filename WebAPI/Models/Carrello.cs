e4f2d4e5602e5a65adf43c5b27fd18463775c397
Salah-eddine KHATIB <salaheddine.khatib@gamil.com>
2018-12-28 15:16:17 +0100

    creazione dei Model: Componente e Carrello

---
+++ WebAPI/Models/Carrello.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    [Table("tbl_carrelli")]
+    public class Carrello
+    {
+        [Key]
+        [Display(Name = "ID Carrello")]
+        public int CarrelloID { get; set; }
+
+        [Required]
+        public string Matricola { get; set; }
+
+        [Required, Range(1900, 2100)]
+        public int AnnoArrivo { get; set; }
+
+        [Required, StringLength(100)]
+        public string AreaStock { get; set; }
+        [Required, StringLength(100)]
+        public string Locazione { get; set; }
+
+ 
+        public virtual ICollection<Componente> Componenti { get; set; }
+
+
+        public Carrello(string matricola, int annoArrivo, string areaStock, string locazione)
+        {
+
+            this.Matricola = matricola;
+            this.AnnoArrivo = annoArrivo;
+            this.AreaStock = areaStock;
+            this.Locazione = locazione;
+        }
+
+    }
+}
---
+++ WebAPI/Models/Componente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    [Table("tbl_componenti")]
+    public class Componente
+    {
+        [Key] 
+        [Display(Name = "ID Componente")]
+        public int ComponenteID { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Codice { get; set; }
+
+        [Required, MinLength(5), MaxLength(70)]
+        public string Descrizione { get; set; }
+
+        public string Note { get; set; }
+
+        public int CarrelloID { get; set; }
+       
+        [ForeignKey("CarrelloID")]
+        public virtual Carrello Carrello { get; set; }
+
+        public Componente()
+        {
+        }
+
+        public Componente(string codice, string descrizione, string note, int carrelloID)
+        {
+            Codice = codice;
+            Descrizione = descrizione;
+            Note = note;
+            CarrelloID = carrelloID;
+        }
+    }
+}
